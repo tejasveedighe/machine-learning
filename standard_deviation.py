a7b7af6eb0dc96ede0714c00d5c82c7ef855ac07
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-09-07 16:43:07 +0530

    standard deviation and variance

---
+++ standard_deviation.py
@@ -0,0 +1,12 @@
+# the standard deviation is the value that tells how much the values in the data are far apart from each other
+
+import numpy as np
+
+data = [32,111,138,28,59,77,97]
+
+print(np.std(data))
+
+
+# variance is also like standard deviation just the method is different, std = sqrt(variance)
+
+print(np.var(data))
